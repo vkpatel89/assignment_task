c4689edc72186b99857e5b1f8075f3e4be04cacc
Vikas Patel <vkpatel.89@gmail.com>
2023-11-30 10:00:22 +0530

    header html and css done

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,1 +1,15 @@
- 
+ const pageObj = {
+    handleHamburger: function(){
+        const hamburger = document.getElementById('hamburgerIcon'),
+              menu = document.getElementById('mainMenu');  
+        hamburger.addEventListener('click', function(){
+            this.classList.toggle('active');
+            menu.classList.toggle('active'); 
+        })
+    }
+ }
+
+ window.onload = function () {
+    pageObj.handleHamburger();
+  }
+  
